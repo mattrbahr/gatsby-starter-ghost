b59991eda4b242015977477ded5f2d638b31fecd
Matt Bahr <mattrbahr@gmail.com>
2021-01-23 08:50:13 -0500

    try again

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === `production` && config.siteUrl === `http://localhos
 */
 module.exports = {
     siteMetadata: {
-        siteUrl: process.env.quantified-ghost.herokuapp.com || config.siteUrl,
+        siteUrl: quantified-ghost.herokuapp.com || config.siteUrl,
     },
     plugins: [
         /**
